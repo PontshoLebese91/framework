89d9162a3ea56df2bd7052bc6f33293a684841c3
Pontsho Lebese <plebese@labournet.com>
2025-11-18 13:43:58 +0200

    add extent report generation to the test, lets see what it doesin the server

--- src/test/java/loginTest.java
+++ src/test/java/loginTest.java
@@ -1,15 +1,22 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import pages.loginPage;
 
 import org.testng.annotations.Test;
 public class loginTest extends base{
 
+    ExtentReports extent;
+    ExtentTest test;
+
     @Test
     public void verifyLoginWorks() {
 
-        /*ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
+        ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
         ExtentReports extent = new ExtentReports();
         extent.attachReporter(spark);
-        ExtentTest test = extent.createTest(this.getClass().getSimpleName());*/
+        extent.setSystemInfo("Tester","Pontsho");
+        ExtentTest test = extent.createTest(this.getClass().getSimpleName());
         try {
             loginPage LoginPage = new loginPage(driver);
             loginPage.enterUsername("admin");
@@ -18,12 +25,14 @@ public class loginTest extends base{
 
             System.out.println("Login test executed successfully.");
 
-            //test.pass("Login test executed successfully.");
+            test.pass("Login test executed successfully.");
+            extent.flush();
         }
         catch(Exception e)
         {
-            //test.fail(e);
+            test.fail(e);
             throw e;
+
         }
     }
 }
